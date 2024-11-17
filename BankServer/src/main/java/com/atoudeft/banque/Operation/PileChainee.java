2a6756d04fe00ff18da8cdfe9277b585a1b0c362
nancynhn <156736092+nancynhn@users.noreply.github.com>
2024-11-17 12:46:32 -0500

    Correction JavaDoc-PileChainee

--- BankServer/src/main/java/com/atoudeft/banque/Operation/PileChainee.java
+++ BankServer/src/main/java/com/atoudeft/banque/Operation/PileChainee.java
@@ -5,11 +5,15 @@ import java.util.ArrayList;
 
 /**
  * La classe PileChainee est une classe qui represente une pile chainee d'operation banacaire
- * Elle permet d'empiler et de depiler les operations bancaire (LIFO)
+ * Elle permet d'empiler et de depiler les operations bancaires (LIFO)
  */
 public class PileChainee implements Serializable {
     private Noeud elementSommet;
 
+    /**
+     * Constructeur par defaut de la classe PileChainee
+     * Initialise une pile chainee vide
+     */
     public PileChainee() {
         this.elementSommet = null;
     }
@@ -17,7 +21,6 @@ public class PileChainee implements Serializable {
     /**
      * Fait par Melissa Sehad
      * Methode qui empile une nouvelle operation au sommet de la pile chainee
-     *
      * @param operation L'operation banncaire a empiler
      */
     public void empiler(Operation operation) {
@@ -25,12 +28,26 @@ public class PileChainee implements Serializable {
         suivant.setNoeudSuivant(elementSommet);
         elementSommet = suivant;
     }
+
+    /**
+     * Fait par Melissa Sehad
+     * Trouve l'operation situee au dessus de la pile chainee, et la retourne
+     * @return  l'operation au dessus de la pile,
+     *          null, si la pile est vide
+     */
     public Operation getSommetDePile(){
         if(elementSommet!=null){
            return elementSommet.getOperation();
         }
         return null;
     }
+
+    /**
+     * Fait par Melissa Sehad
+     * Recupere l'historique des operations de l'utilisateur, et la retourne
+     * @return liste contenant les operations du client
+     *         liste vide si la pile est vide
+     */
     public ArrayList<Operation> getHistoriqueOperation(){
         ArrayList<Operation> historiqueOperation = new ArrayList<>();
         while(elementSommet!=null){
@@ -43,7 +60,6 @@ public class PileChainee implements Serializable {
     /**
      * Fait par Melissa Sehad
      * Methode qui verifie et retourne si la pile est vide
-     *
      * @return true si la pile chainee est vide, sinon false
      */
     public boolean estVide() {
