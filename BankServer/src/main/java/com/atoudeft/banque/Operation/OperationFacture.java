982f927554ac445b3fedf1fc2e0b3c5874a5de7a
nancynhn <156736092+nancynhn@users.noreply.github.com>
2024-11-17 01:25:08 -0500

    Correction JavaDoc-OperationFacture

--- BankServer/src/main/java/com/atoudeft/banque/Operation/OperationFacture.java
+++ BankServer/src/main/java/com/atoudeft/banque/Operation/OperationFacture.java
@@ -4,6 +4,9 @@ import com.atoudeft.banque.TypeOperation;
 
 /**
  * La classe OperationFacture est une classe qui represente les operations de paiement de facture
+ * Elle est une sous-classe de la classe Operation et stocke les détails du paiement de facture;
+ *       - sa date de transaction, le type de transaction, et le montant de transaction
+ * @author Mathis Odjo'o Ada
  */
 public class OperationFacture extends Operation {
     private final double montantFacture;
@@ -12,8 +15,7 @@ public class OperationFacture extends Operation {
 
     /**
      * Fait par Mathis Odjo'o Ada
-     * Constructeur de l'operation de paiement de facture
-     *
+     * Constructeur de l'opération de paiement de facture
      * @param montantFacture     le montant de la facture
      * @param numeroFacture      le numero de la facture
      * @param descriptionFacture la description de la facture
@@ -28,7 +30,6 @@ public class OperationFacture extends Operation {
     /**
      * Fait par Melissa Sehad
      * Methode qui retourne le montant de la facture
-     *
      * @return le montant de la facture
      */
     public double getMontantFacture() {
@@ -38,7 +39,6 @@ public class OperationFacture extends Operation {
     /**
      * Fait par Melissa Sehad
      * Methode qui retourne la numero de la facture
-     *
      * @return le numero de la facture
      */
     public String getNumeroFacture() {
@@ -48,17 +48,19 @@ public class OperationFacture extends Operation {
     /**
      * Fait par Melissa Sehad
      * Methode qui retourne la description de la facture
-     *
      * @return la description de la facture
      */
     public String getDescriptionFacture() {
         return descriptionFacture;
     }
 
-    //Q7.3 - Fait par Nancy Nguyen
+    /**
+     * Q7.3 - Fait par Nancy Nguyen et Melissa Sehad
+     * Décrit l'opération de paiement de facture, incluant la date, le type, et le montant du paiement de facture
+     * @return chaîne de format: DATE: [date] TYPE: [type] MONTANT: [montant]
+     */
     public String toString() {
-        return " Operation Facture ( "
-                + "DATE: " + getDate()
+        return    "DATE: " + getDate()
                 + "    TYPE: " + this.getType()
                 + "    MONTANT: " + this.montantFacture + " )";
     }
