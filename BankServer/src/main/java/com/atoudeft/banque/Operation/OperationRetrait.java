9fe22d57684b0bbb0d60935bd2f0b16bc47a153e
nancynhn <156736092+nancynhn@users.noreply.github.com>
2024-11-17 01:23:31 -0500

    Correction JavaDoc-OperationRetrait

--- BankServer/src/main/java/com/atoudeft/banque/Operation/OperationRetrait.java
+++ BankServer/src/main/java/com/atoudeft/banque/Operation/OperationRetrait.java
@@ -1,11 +1,13 @@
 package com.atoudeft.banque.Operation;
 
 import com.atoudeft.banque.TypeOperation;
-
 import java.io.Serializable;
 
 /**
- * La classe OperationRetrait est une classe qui represente les operation de retrait bancaire
+ * La classe OperationRetrait est une classe qui représente les opérations de retrait bancaire
+ * Elle est une sous-classe de la classe Operation et stocke les détails du retrait bancaire ;
+ *        - sa date de transaction, le type de transaction, et le montant de transaction
+ * @author Mathis Odjo'o Ada
  */
 public class OperationRetrait extends Operation implements Serializable {
     private final double montantRetrait;
@@ -13,7 +15,6 @@ public class OperationRetrait extends Operation implements Serializable {
     /**
      * Fait par Mathis Odjo'o Ada
      * Constructeur de l'opération de retrait bancaire
-     *
      * @param montantRetrait Le type de l'Opération que le client veut exécuter
      */
     public OperationRetrait(double montantRetrait) {
@@ -24,16 +25,19 @@ public class OperationRetrait extends Operation implements Serializable {
     /**
      * Fait par Melissa Sehad
      * Methode qui retourne le montant de retrait
-     *
      * @return le montant du retrait bancaire
      */
     public double getMontantRetrait() {
         return montantRetrait;
     }
 
-    //Q7.3 - Fait par Nancy Nguyen
+    /**
+     * Q7.3 - Fait par Nancy Nguyen et Melissa Sehad
+     * Décrit l'opération de retrait, incluant la date, le type, et le montant du retrait
+     * @return chaîne de format:  DATE: [date] TYPE: [type] MONTANT: [montant]
+     */
     public String toString() {
-        return "Retrait bancaire ( " + "DATE: " + getDate()
+        return    "DATE: " + getDate()
                 + "    TYPE: " + this.getType()
                 + "    MONTANT: " + this.montantRetrait + " )";
     }
