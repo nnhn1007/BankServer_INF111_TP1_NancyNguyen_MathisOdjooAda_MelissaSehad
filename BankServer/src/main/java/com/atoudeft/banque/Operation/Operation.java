ea9ec9c5fc456b0c4fca7eca52176c88609ee06d
nancynhn <156736092+nancynhn@users.noreply.github.com>
2024-11-17 01:20:26 -0500

    Correction JavaDoc - Operation

--- BankServer/src/main/java/com/atoudeft/banque/Operation/Operation.java
+++ BankServer/src/main/java/com/atoudeft/banque/Operation/Operation.java
@@ -7,7 +7,8 @@ import java.util.Date;
 
 /**
  * La classe Operation est une classe qui represente une operation bancaire
- * Elle est la base pour les classe OperationDepot, OperationFacture, OperationRetrait et OperationTranfer
+ * Elle est la classe parent des classe OperationDepot, OperationFacture, OperationRetrait et OperationTranfer
+ * @author Mathis Odjo'o Ada
  */
 public abstract class Operation implements Serializable {
     private final TypeOperation type;
@@ -17,19 +18,16 @@ public abstract class Operation implements Serializable {
      * Fait par Mathis Odjo'o Ada
      * Constructeur qui initialise le type de l'opération
      * et la date de l'heure actuelle.
-     *
      * @param type Le type de l'Opération que le client veut exécuter
      */
     public Operation(TypeOperation type) {
         this.type = type;
-        this.date = new Date(System.currentTimeMillis()); // Date de l'opération
+        this.date = new Date(System.currentTimeMillis());
     }
 
     /**
-     * Fait par Mathis Odjo'o Ada
-     * À vérifier puisque ce n'est pas demandé dans l'énoncé.
+     * Fait par Mathis Odjo'o Ada et Nancy Nguyen
      * Methode qui retourne le type de l'operation
-     *
      * @return type
      */
     public TypeOperation getType() {
@@ -37,10 +35,8 @@ public abstract class Operation implements Serializable {
     }
 
     /**
-     * Fait par Mathis Odjo'o Ada
-     * À vérifier puisque ce n'est pas demandé dans l'énoncé.
+     * Fait par Mathis Odjo'o Ada et Nancy Nguyen
      * Methode qui retourne la date de l'operation et l'heure de l'operation
-     *
      * @return date
      */
     public Date getDate() {
