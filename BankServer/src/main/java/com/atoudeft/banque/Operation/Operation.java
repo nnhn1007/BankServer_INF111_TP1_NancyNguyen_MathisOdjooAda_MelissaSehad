05cf581caad661cc967f07e42770b43878304d80
nancynhn <156736092+nancynhn@users.noreply.github.com>
2024-11-17 17:37:41 -0500

    Correction JavaDoc-Operation

--- BankServer/src/main/java/com/atoudeft/banque/Operation/Operation.java
+++ BankServer/src/main/java/com/atoudeft/banque/Operation/Operation.java
@@ -1,13 +1,12 @@
 package com.atoudeft.banque.Operation;
 
 import com.atoudeft.banque.TypeOperation;
-
 import java.io.Serializable;
 import java.util.Date;
 
 /**
- * La classe Operation est une classe qui represente une operation bancaire
- * Elle est la classe parent des classe OperationDepot, OperationFacture, OperationRetrait et OperationTranfer
+ * Operation représente les différents opérations bancaires possibles; OperationDepot, OperationFacture,
+ * OperationRetrait et OperationTranfer, qui sont tous des sous-classes d'Operation.
  * @author Mathis Odjo'o Ada
  */
 public abstract class Operation implements Serializable {
